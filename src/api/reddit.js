7631345b250fcd6fb2027c38afc3a3d366602ff8
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-11-28 18:28:38 -0500

    get news of Api

---
+++ src/api/reddit.js
@@ -0,0 +1,12 @@
+import { API_URL } from "../utils/constants";
+
+export const getNewsAPI = async () => {
+  try {
+    const url = `${API_URL}/hot.json`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/screens/News.js
+++ src/screens/News.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView, Text } from "react-native";
+import { getNewsAPI } from "../api/reddit";
 
 const News = () => {
+  useEffect(() => {
+    (async () => {
+      await loadNews();
+    })();
+  }, []);
+
+  const loadNews = async () => {
+    try {
+      const response = await getNewsAPI();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>News</Text>
